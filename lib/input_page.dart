cc8ed2ded5ab77e65526c226844ae14e03b4ddeb
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2022-01-06 00:18:44 +0530

    refactoring input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xff1d1e33);
+
 class Inputpage extends StatefulWidget {
   const Inputpage({Key? key}) : super(key: key);
 
@@ -13,10 +16,70 @@ class _InputpageState extends State<Inputpage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('BMI Calculator'),
+        title: const Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text('Hello world!'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReuseCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReuseCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReuseCard(
+              colour: activeCardColor,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReuseCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReuseCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            color: Colors.red,
+            height: bottomContainerHeight,
+            width: double.infinity,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReuseCard extends StatelessWidget {
+  final Color colour;
+  const ReuseCard({Key? key, required this.colour}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
